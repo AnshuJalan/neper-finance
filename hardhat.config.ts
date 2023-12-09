9c6577fb60f45749969491103576034b05956dab
AnshuJalan <anshujalan206@gmail.com>
2023-12-09 07:05:34 +0530

    feat: add deploy networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,43 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 11155111,
+    },
+    arbitrum: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 421614,
+    },
+    polygon: {
+      url: "https://rpc.public.zkevm-test.net",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 1442,
+    },
+    scroll: {
+      url: "https://scroll-sepolia.blockpi.network/v1/rpc/public",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 534351,
+    },
+    base: {
+      url: "https://base-goerli.publicnode.com",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 84531,
+    },
+    celo: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 44787,
+    },
+  },
 };
 
 export default config;
