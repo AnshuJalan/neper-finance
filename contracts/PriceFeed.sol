009f79f02db6cedbc7c71c097bfd533643dd948d
AnshuJalan <anshujalan206@gmail.com>
2023-12-09 06:55:19 +0530

    feat: add pricefeed intermediary

---
+++ contracts/PriceFeed.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "./dependencies/Ownable.sol";
+import "./interfaces/IPriceFeed.sol";
+import "./interfaces/chainlink/IAggregatorV3Interface.sol";
+
+contract PriceFeed is Ownable, IPriceFeed {
+    struct Slot0 {
+        uint128 price;
+        uint128 updatedAt;
+    }
+
+    uint constant SCALE = 8;
+
+    Slot0 internal slot0;
+    IAggregatorV3Interface internal chainlinkFeed;
+
+    function setAddresses(address oracleAddress) external onlyOwner {
+        chainlinkFeed = IAggregatorV3Interface(oracleAddress);
+    }
+
+    function updatePrice() external {
+        (, int answer, , uint timeStamp, ) = chainlinkFeed.latestRoundData();
+        slot0 = Slot0(uint128(uint(answer)), uint128(timeStamp));
+    }
+
+    function getPrice() external view returns (uint128) {
+        Slot0 memory _slot0 = slot0;
+        return _slot0.price;
+    }
+
+    function getScale() external pure returns (uint) {
+        return SCALE;
+    }
+}
